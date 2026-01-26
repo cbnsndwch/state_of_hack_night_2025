1313bd1691365b7ca2d5f58cacff89c44c3fcc3f
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2026-01-26 02:34:01 -0500

    fix: import direct css file from inter package

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@fontsource/inter';
+
+import '@fontsource/inter/index.css';
+
 import './index.css';
 import App from './App.tsx';
 
