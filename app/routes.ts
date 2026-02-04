20f879caad825ebac3ca7f0b49fc1f78d00cf881
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2026-02-03 23:35:11 -0500

    refactor(routes): update route definitions

--- app/routes.ts
+++ app/routes.ts
@@ -12,5 +12,9 @@ export default [
     // api routes
     route('api/webhooks/luma', 'routes/api/webhooks/luma.ts'),
     route('api/auth/check-user', 'routes/api/auth/check-user.ts'),
-    route('api/auth/complete-login', 'routes/api/auth/complete-login.ts')
+    route('api/auth/complete-login', 'routes/api/auth/complete-login.ts'),
+    route('api/profile', 'routes/api/profile.ts'),
+    route('api/events', 'routes/api/events.ts'),
+    route('api/demo-slots', 'routes/api/demo-slots.ts'),
+    route('api/projects-list', 'routes/api/projects-list.ts')
 ] satisfies RouteConfig;
