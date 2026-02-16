1feb7669d34b647854bb7f84efacc34f9026e2f9
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2026-02-16 00:13:49 -0500

    build(deps): add react-router-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
+import path from 'node:path';
+
 import { defineConfig } from 'vite';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+import { reactRouterDevTools } from 'react-router-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [reactRouter(), tailwindcss()],
+    plugins: [reactRouterDevTools(), reactRouter(), tailwindcss()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './app'),
